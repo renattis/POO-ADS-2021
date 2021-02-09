1602e7ba799e9875d56ac446a542c60bbf306b8d
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:41:56 -0300

    Create Q02.py

---
+++ Semana04/Q02.py
@@ -0,0 +1,8 @@
+public class q2 {
+    public static void main(String[] args) {
+        Conta[] contas = new Conta[5];
+        contas[0].setSaldo(10);
+        System.out.println(contas[0].getSaldo);
+    }
+    
+}
