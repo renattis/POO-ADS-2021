e0990eb33b74d7c869156e41cdc46736fc0776b7
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 07:49:54 -0300

    Create conta.java

---
+++ semana07/q01/conta.java
@@ -0,0 +1,32 @@
+public class Conta {
+	// a - Atributos privados e métodos públicos
+	private String numero;
+	private double saldo;
+
+	public Conta(String numero, double valor) {
+		this.numero = numero;
+		this.saldo = valor;
+	}
+
+	public void sacar(double valor) {
+		saldo = saldo - valor;
+	}
+
+	public void depositar(double valor) {
+		saldo = saldo + valor;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void transferir(Conta destino, double valor) {
+		this.sacar(valor);
+		destino.depositar(valor);
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+}
