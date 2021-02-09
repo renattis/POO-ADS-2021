cb8eb3ee26498904e2e7cf7f9dbee6f674d07d4b
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:28:47 -0300

    Create Q10.py

---
+++ semana2/Q10.py
@@ -0,0 +1,9 @@
+public class TestaSoma {
+    public static void main(String[] args) {
+        Soma s = new Soma();
+        s.a = 2;
+        s.b = 3;
+
+        System.out.println(s.calculaSoma());
+    }
+}
