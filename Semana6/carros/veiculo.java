43e3076f839c89713faa3db798358b82533bba35
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 07:40:01 -0300

    Create veiculo.java

---
+++ Semana6/carros/veiculo.java
@@ -0,0 +1,40 @@
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private int ano;
+
+    public Veiculo(String placa, String modelo, int ano) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    // Exibindo Atributos
+    public void exibir(){
+        System.out.println("Placa: " +getPlaca() + "Modelo" +getModelo() + "Ano" + getAno());
+    }
+}
