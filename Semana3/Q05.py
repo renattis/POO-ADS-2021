9d004c56efbc4a4177ac9135d3537fd14cb588de
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:36:54 -0300

    Create Q05.py

---
+++ Semana3/Q05.py
@@ -0,0 +1,9 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+public class TestaRadio {
+    public static void main(String[] args) {
+        Radio r = new Radio();
+        r.volume = 10;
+    }
+
+}
