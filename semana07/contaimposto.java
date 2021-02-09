0e8a99fdbcd6b44bf2fee822fe270bee699a0757
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 07:52:33 -0300

    Create contaimposto.java

---
+++ semana07/contaimposto.java
@@ -0,0 +1,13 @@
+public class ContaImposto extends Conta {
+	private double taxa = 0.38 / 100;
+
+	public ContaImposto(String numero, double valor) {
+		super(numero, valor);
+	}
+
+	@Override
+	public void sacar(double valor) {
+		super.sacar(valor);
+		super.sacar(valor * taxa);
+	}
+}
