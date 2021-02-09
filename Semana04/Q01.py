765bf21e6f1e049c1c9bf6cb1d40b03a307ad154
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:41:32 -0300

    Create Q01.py

---
+++ Semana04/Q01.py
@@ -0,0 +1,13 @@
+public class q1 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[5];
+        for(int i = 0; i <= a.length; i++){
+            a[i] = i;
+            System.out.println(a[i]);
+        }
+    
+    }
+    
+}
