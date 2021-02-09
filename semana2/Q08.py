3ed05722e92b3dc9b143d35498362267a1179335
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:27:16 -0300

    Create Q08.py

---
+++ semana2/Q08.py
@@ -0,0 +1,9 @@
+public class Saudacao {
+    String texto;
+    String destinatario;
+
+    // Método
+    public String obterSaudacao() {
+        System.out.println(texto + " " + destinatario);
+    }
+}
