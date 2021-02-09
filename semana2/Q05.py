de05b07906ddc77f53adcc3d8ea0c841e019b222
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-02-09 05:25:06 -0300

    Create Q05.py

---
+++ semana2/Q05.py
@@ -0,0 +1,15 @@
+// public class Exemplo {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 1;
+//         System.out.println("Soma: " + a + b);
+//     }
+// }
+public class Exemplo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        int soma = a + b;
+        System.out.println("Soma: " + soma);
+    }
+}
